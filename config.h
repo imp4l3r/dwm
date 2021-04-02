2eeb0d2095962f6b9d8b1bef6fa39ef75164e71d
imp4l3r <sogtrhs@gmail.com>
2021-04-02 12:48:58 +0200

    updated config

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 4;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 //	[SchemeSel]  = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },
 };
 
 /* Custom keybindings */
@@ -40,7 +40,7 @@ static const char *muteVolume[] = {"volumecontrol.sh", "muteVol", NULL};
 static const char *muteMicrophone[] = {"volumecontrol.sh", "muteMic", NULL};
 static const char *increaseBacklight[] = {"brightness", "raise", NULL};
 static const char *decreaseBacklight[] = {"brightness", "lower", NULL};
-
+static const char *acpicallerdmenu[] = {"acpicaller-dmenu", NULL};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -81,7 +81,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
 static char *statuscmds[] = { "notify-send Mouse$BUTTON" };
@@ -148,6 +148,7 @@ static Key keys[] = {
 	{ 0,							XF86XK_AudioMute,        spawn,  {.v = muteVolume } },
 	{ 0,                            XF86XK_AudioMicMute,     spawn,  {.v = muteMicrophone } },
 	{ 0,							XK_Print, spawn,           {.v = flameshotscreenshot} },
+	{ MODKEY,                            XK_p, spawn,               {.v = acpicallerdmenu} },
 };
 
 /* button definitions */
